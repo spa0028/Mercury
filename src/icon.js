106d2c328e485db6da5328ba650e2de99ea82b81
spa0028 <spa0028@bddc.edu.au>
2024-07-25 19:41:13 +1000

    Icons are cached and thus only load once

--- src/icon.js
+++ src/icon.js
@@ -1,4 +1,6 @@
 var globalIconCount = 0;
+var pathCache = {}
+var waitingFor = {}
 
 class pathIcon extends HTMLElement {
     constructor() {
@@ -9,44 +11,64 @@ class pathIcon extends HTMLElement {
     load() {
         if (this.hasAttribute("src"))
             {
-                fetch(this.attributes.src.value).then(e=>{return e.text()}).then(text=>{
-                    let iconSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg")
-                    
-                    iconSVG.setAttribute("viewBox", "0 0 1 1")
-
-                    let path = document.createElementNS("http://www.w3.org/2000/svg","path")
-                    path.attributes.setNamedItem(document.createAttribute("d"))
-                    path.setAttribute("d", text)
-                    path.setAttribute("transform", "scale(0.8, 0.8) translate(0.1, 0.1)")
-
-                    globalIconCount++
-
-                    if (this.hasAttribute("invertable"))
+                let source = this.attributes.src.value
+                if (pathCache.hasOwnProperty(source)) {
+                    this.applyPath(pathCache[source])
+                } else {
+                    if (waitingFor.hasOwnProperty(source))
                     {
-                        let colourRect = iconSVG.appendChild(document.createElementNS("http://www.w3.org/2000/svg","rect"))
-                        colourRect.setAttribute("x", 0)
-                        colourRect.setAttribute("y", 0)
-                        colourRect.setAttribute("mask", `url(#iconMask${globalIconCount})`)
-                        colourRect.style.fill = "var(--themeAccent)"
-                        colourRect.style.height = colourRect.style.width = "1"
-        
-                        let mask = iconSVG.appendChild(document.createElementNS("http://www.w3.org/2000/svg","mask"))
-                        mask.id = `iconMask${globalIconCount}`
-        
-                        let maskRect = mask.appendChild(document.createElementNS("http://www.w3.org/2000/svg","rect"))
-                        maskRect.setAttribute("x", 0)
-                        maskRect.setAttribute("y", 0)
-                        maskRect.style.height = maskRect.style.width = "1"
-
-                        mask.appendChild(path)
-                    } else {
-                        path.style.fill = "var(--themeAccent)"
-
-                        iconSVG.appendChild(path)
+                        waitingFor[source].push(this)
                     }
-                    this.appendChild(iconSVG)
-                })
+                    else {
+                        waitingFor[source] = []
+                        fetch(source).then(e=>{return e.text()}).then(pathText=>{
+                            pathCache[source] = pathText
+                            this.applyPath(pathText)
+                            waitingFor[source].forEach(waiting => {
+                                waiting.applyPath(pathText)
+                            });
+                        })
+                    }
+                }
             }
+    }
+
+    applyPath(pathText) {
+        let iconSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg")
+                    
+        iconSVG.setAttribute("viewBox", "0 0 1 1")
+
+        let path = document.createElementNS("http://www.w3.org/2000/svg","path")
+        path.attributes.setNamedItem(document.createAttribute("d"))
+        path.setAttribute("d", pathText)
+        path.setAttribute("transform", "scale(0.8, 0.8) translate(0.1, 0.1)")
+
+        globalIconCount++
+
+        if (this.hasAttribute("invertable"))
+        {
+            let colourRect = iconSVG.appendChild(document.createElementNS("http://www.w3.org/2000/svg","rect"))
+            colourRect.setAttribute("x", 0)
+            colourRect.setAttribute("y", 0)
+            colourRect.setAttribute("mask", `url(#iconMask${globalIconCount})`)
+            colourRect.style.fill = "var(--themeAccent)"
+            colourRect.style.height = colourRect.style.width = "1"
+
+            let mask = iconSVG.appendChild(document.createElementNS("http://www.w3.org/2000/svg","mask"))
+            mask.id = `iconMask${globalIconCount}`
+
+            let maskRect = mask.appendChild(document.createElementNS("http://www.w3.org/2000/svg","rect"))
+            maskRect.setAttribute("x", 0)
+            maskRect.setAttribute("y", 0)
+            maskRect.style.height = maskRect.style.width = "1"
+
+            mask.appendChild(path)
+        } else {
+            path.style.fill = "var(--themeAccent)"
+
+            iconSVG.appendChild(path)
+        }
+        this.appendChild(iconSVG)
     }
 }
 
